8697f4cb278cfb60b685cab26e96a08090d66830
2giduck <cosmos7256@naver.com>
2022-11-02 23:21:32 +0900

    221102-1

---
+++ src/leetcode/_1365.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class _1365 {
+    public static void main(String[] args) {
+        int[] n1 = new int[]{8,1,2,2,3},
+                n2 = new int[] {6, 5, 4, 8},
+                n3 = new int[] {7, 7, 7, 7};
+
+        System.err.println(Arrays.toString(smallerNumbersThanCurrent(n1)));
+        System.err.println(Arrays.toString(smallerNumbersThanCurrent(n2)));
+        System.err.println(Arrays.toString(smallerNumbersThanCurrent(n3)));
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] sortArr = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sortArr);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < sortArr.length; i++) {
+            if(!map.containsKey(sortArr[i])) {
+                map.put(sortArr[i], i);
+            }
+        }
+
+        var result = new int[nums.length];
+        int count = 0;
+        for(int n : nums) {
+            result[count++] = map.get(n);
+        }
+
+        return result;
+    }
+}

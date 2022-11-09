3b8003c4895437fa7fd627738e56c8ad8c2599b8
2giduck <cosmos7256@naver.com>
2022-11-09 22:33:44 +0900

    221109-1

---
+++ src/leetcode/re_1379.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class re_1379 {
+
+    public static void main(String[] args) {
+
+    }
+
+     public static class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        return null;
+    }
+}

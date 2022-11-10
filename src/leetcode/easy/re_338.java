eab6bf0cc90476c00e375296c3f233a0a52fa17e
2giduck <cosmos7256@naver.com>
2022-11-10 22:51:07 +0900

    221110-1

---
+++ src/leetcode/easy/re_338.java
@@ -0,0 +1,17 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+public class re_338 {
+    public static void main(String[] args) {
+        int n1 = 2, n2 = 5;
+        System.err.println(Arrays.toString(countBits(n1)));
+        System.err.println(Arrays.toString(countBits(n2)));
+    }
+
+    public static int[] countBits(int n) {
+        int[] answer = new int[n+1];
+
+        return null;
+    }
+}

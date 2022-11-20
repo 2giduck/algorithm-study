8828c3a3667f7acd2d9e3e0abd054239b26cdfc2
2giduck <cosmos7256@naver.com>
2022-11-20 12:08:36 +0900

    221120-2

---
+++ src/programmers/etc/re_마법_엘리베이터.java
@@ -0,0 +1,15 @@
+package programmers.etc;
+
+public class re_마법_엘리베이터 {
+    public static void main(String[] args) {
+        int s1 = 16, s2 = 2554;
+        System.err.println(solution(s1));
+        System.err.println(solution(s2));
+    }
+
+    public static int solution(int storey) {
+        int answer = 0;
+
+        return answer;
+    }
+}

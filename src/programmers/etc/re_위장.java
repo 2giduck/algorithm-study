57639e221806326510d7a05f6233fdc4b1d36620
2giduck <cosmos7256@naver.com>
2022-11-30 22:21:09 +0900

    221130-1

---
+++ src/programmers/etc/re_위장.java
@@ -0,0 +1,17 @@
+package programmers.etc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class re_위장 {
+    public static void main(String[] args) {
+        String[][] c1 = new String[][] {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}},
+                c2 = new String[][] {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
+        System.err.println(solution(c1));
+        System.err.println(solution(c2));
+    }
+
+    public static int solution(String[][] clothes) {
+        return 0;
+    }
+}

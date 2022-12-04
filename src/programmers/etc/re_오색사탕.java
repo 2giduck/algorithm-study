6f78d5bd85dabb69bcd2f5e5fa621867b897b15e
2giduck <cosmos7256@naver.com>
2022-12-04 20:36:39 +0900

    221204-2

---
+++ src/programmers/etc/re_오색사탕.java
@@ -0,0 +1,12 @@
+package programmers.etc;
+
+public class re_오색사탕 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(String candy, int[] positions) {
+        int[] answer = {};
+        return answer;
+    }
+}

b4e76f60235d28fb0126b8942444a958e7ab2053
2giduck <cosmos7256@naver.com>
2022-12-05 22:32:09 +0900

    221205-2

---
+++ src/leetcode/medium/re_692.java
@@ -0,0 +1,17 @@
+package leetcode.medium;
+
+import java.util.*;
+
+public class re_692 {
+    public static void main(String[] args) {
+        String[] w1 = new String[] {"i","love","leetcode","i","love","coding" },
+                w2 = new String[] {"the","day","is","sunny","the","the","the","sunny","is","is"};
+        int k1 = 2, k2 = 4;
+        System.err.println(topKFrequent(w1, k1));
+        System.err.println(topKFrequent(w2, k2));
+    }
+
+    public static List<String> topKFrequent(String[] words, int k) {
+        return null;
+    }
+}

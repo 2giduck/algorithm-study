176ef7b8bcf82dc6d38f8be3ab57eb44e0670b8c
2giduck <cosmos7256@naver.com>
2022-12-06 22:46:31 +0900

    221206-2

---
+++ src/leetcode/medium/re_394.java
@@ -0,0 +1,14 @@
+package leetcode.medium;
+
+public class re_394 {
+    public static void main(String[] args) {
+        String s1 = "3[a]2[bc]", s2 = "3[a2[c]]", s3 = "2[abc]3[cd]ef";
+        System.err.println(decodeString(s1));
+        System.err.println(decodeString(s2));
+        System.err.println(decodeString(s3));
+    }
+
+    public static String decodeString(String s) {
+        return null;
+    }
+}

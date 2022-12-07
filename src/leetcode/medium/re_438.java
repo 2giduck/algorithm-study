a164f213acfa55705ed376658fa5c006183dda77
2giduck <cosmos7256@naver.com>
2022-12-07 22:39:34 +0900

    221207-2

---
+++ src/leetcode/medium/re_438.java
@@ -0,0 +1,18 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class re_438 {
+    public static void main(String[] args) {
+        String s1 = "cbaebabacd", s2 = "abab";
+        String p1 = "abc", p2 = "ab";
+        System.err.println(findAnagrams(s1, p1));
+        System.err.println(findAnagrams(s2, p2));
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        return null;
+    }
+}

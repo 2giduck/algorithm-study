f320a0093497b9588c7274a81534dc3060cee6f1
2giduck <cosmos7256@naver.com>
2022-12-08 22:30:26 +0900

    221208-1

---
+++ src/leetcode/medium/re_424.java
@@ -0,0 +1,15 @@
+package leetcode.medium;
+
+public class re_424 {
+    public static void main(String[] args) {
+        String s1 = "ABAB", s2 = "AABABBA";
+        int k1 = 2, k2 = 1;
+
+        System.err.println(characterReplacement(s1, k1));
+        System.err.println(characterReplacement(s2, k2));
+    }
+
+    public static int characterReplacement(String s, int k) {
+        return 0;
+    }
+}

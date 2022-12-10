0c1b4df6df11ab5d6f54bb0a2b26d6352588446d
2giduck <cosmos7256@naver.com>
2022-12-10 13:39:32 +0900

    221210-1

---
+++ src/leetcode/medium/_62.java
@@ -0,0 +1,19 @@
+package leetcode.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _62 {
+    public static void main(String[] args) {
+        int m1 = 3, n1 = 7;
+        int m2 = 3, n2 = 2;
+        int m3 = 1, n3 = 1;
+        System.err.println(uniquePaths(m1, n1));
+        System.err.println(uniquePaths(m2, n2));
+        System.err.println(uniquePaths(m3, n3));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        return 0;
+    }
+}

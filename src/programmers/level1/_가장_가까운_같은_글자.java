81523d6f01935cde6c96a513c972dfc3dc9a060e
2giduck <41426853+2giduck@users.noreply.github.com>
2022-12-14 16:49:21 +0900

    221214-2

---
+++ src/programmers/level1/_가장_가까운_같은_글자.java
@@ -0,0 +1,19 @@
+package programmers.level1;
+
+public class _가장_가까운_같은_글자 {
+  public int[] solution(String s) {
+          int[] answer = new int[s.length()];
+          HashMap<Character, Integer> map = new HashMap<>();
+
+          for(int i = 0; i<s.length(); i++) {
+              if(map.containsKey(s.charAt(i))) {
+                  answer[i] = i - map.get(s.charAt(i));
+              }else {
+                  answer[i] = -1;
+              }
+              map.put(s.charAt(i), i);
+          }
+
+          return answer;
+      }
+}

b33d86bd74b749c55f897a0b4d787d26772a4716
2giduck <cosmos7256@naver.com>
2022-12-28 20:48:39 +0900

    221228-1

---
+++ src/leetcode/easy/re_242.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+import java.util.HashMap;
+
+public class re_242 {
+    public static void main(String[] args) {
+        String s1 = "anagram", s2 = "rat";
+        String t1 = "nagaram", t2 = "car";
+        System.err.println(isAnagram(s1, t1));
+        System.err.println(isAnagram(s2, t2));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        return true;
+    }
+}

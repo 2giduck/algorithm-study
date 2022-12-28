f93e272cf9eddd05fe52d7bbc52d5a93aad4a396
2giduck <cosmos7256@naver.com>
2022-12-28 21:16:50 +0900

    221228-2

---
+++ src/leetcode/easy/re_235.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+public class re_235 {
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+}

d621760f8741f483194471ed43e4838a27e13251
2giduck <cosmos7256@naver.com>
2023-01-08 23:04:50 +0900

    230108-1

---
+++ src/leetcode/medium/re_542.java
@@ -0,0 +1,19 @@
+package leetcode.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class re_542 {
+    public static void main(String[] args) {
+        int[][] m1 = new int[][] {{0,0,0}, {0,1,0}, {0,0,0}},
+                m2 = new int[][] {{0,0,0}, {0,1,0}, {1,1,1}};
+
+        updateMatrix(m1);
+        updateMatrix(m2);
+    }
+
+    public static int[][] updateMatrix(int[][] mat) {
+        return null;
+    }
+
+}

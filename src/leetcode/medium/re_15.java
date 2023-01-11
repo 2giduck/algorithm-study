1c5094f0dfd8b0ddff22c4ca624a333e950c61a9
2giduck <cosmos7256@naver.com>
2023-01-11 22:28:56 +0900

    230111-1

---
+++ src/leetcode/medium/re_15.java
@@ -0,0 +1,20 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class re_15 {
+    public static void main(String[] args) {
+        int[] n1 = {-1,0,1,2,-1,-4},
+        n2 = {0,1,1},
+        n3 = {0,0,0} ;
+
+        System.err.println(threeSum(n1));
+        System.err.println(threeSum(n2));
+        System.err.println(threeSum(n3));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+}

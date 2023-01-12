9dca0dd2a5568b359106aafc36a69684501002c9
2giduck <cosmos7256@naver.com>
2023-01-12 21:17:06 +0900

    230112-2

--- src/leetcode/easy/re_876.java
+++ src/leetcode/easy/re_876.java
@@ -1,7 +1,24 @@
 package leetcode.easy;
 
 public class re_876 {
-    public static void main(String[] args) {
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 
+    public ListNode middleNode(ListNode head) {
+        if(head == null) return null;
+
+        var slow = head;
+        var fast = head;
+        while( fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 }

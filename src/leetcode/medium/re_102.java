ec242d8a722f9187cbe6331cf15169cd1b41cf6d
2giduck <cosmos7256@naver.com>
2023-01-12 21:11:42 +0900

    230112-1

---
+++ src/leetcode/medium/re_102.java
@@ -0,0 +1,23 @@
+package leetcode.medium;
+
+import java.util.List;
+
+public class re_102 {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}

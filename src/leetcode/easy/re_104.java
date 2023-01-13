31ad6a74d655aa8e514389bcfc1e10abb8486fe5
2giduck <cosmos7256@naver.com>
2023-01-13 22:09:01 +0900

    230113-2

--- src/leetcode/easy/re_104.java
+++ src/leetcode/easy/re_104.java
@@ -1,4 +1,29 @@
 package leetcode.easy;
 
 public class re_104 {
+
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int maxDepth(TreeNode root) {
+        return getMaxLength(root);
+    }
+
+    public int getMaxLength(TreeNode root) {
+        if(root == null) return 0;
+
+        int l = getMaxLength(root.left) + 1;
+        int r = getMaxLength(root.right) + 1;
+        return Math.max(l, r);
+    }
 }

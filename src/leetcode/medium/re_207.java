a7c0238023e35c17c20db9d3a62cceeaa7748855
2giduck <cosmos7256@naver.com>
2023-01-19 23:11:57 +0900

    230119-1

---
+++ src/leetcode/medium/re_207.java
@@ -0,0 +1,23 @@
+package leetcode.medium;
+
+import java.util.*;
+
+public class re_207 {
+    public static void main(String[] args) {
+        int n1 = 2, n2 = 2;
+        int[][] p1 = {{1,0}}, p2 = {{1,0}, {0,1}};
+        int n3 = 3;
+        int[][] p3 = {{1,0}, {1,2}, {0,1}};
+        int n4 = 5;
+        int[][] p4 = {{1,4}, {2,4}, {3,1}, {3,2}};
+        System.err.println(canFinish(n1, p1));
+        System.err.println(canFinish(n2, p2));
+        System.err.println(canFinish(n3, p3));
+        System.err.println(canFinish(n4, p4));
+    }
+
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        return true;
+    }
+}

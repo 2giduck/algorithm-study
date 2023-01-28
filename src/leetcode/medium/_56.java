b9a55d0e6f64627d332ba753556efeb4f4ab51b2
2giduck <cosmos7256@naver.com>
2023-01-28 11:29:28 +0900

    230104-2

---
+++ src/leetcode/medium/_56.java
@@ -0,0 +1,12 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _56 {
+    public int[][] merge(int[][] intervals) {
+
+        List<int[]> list = new ArrayList<>();
+        return list.toArray(new int[0][]);
+    }
+}
--- src/leetcode/medium/re_11.java
+++ src/leetcode/medium/re_11.java
@@ -2,6 +2,6 @@ package leetcode.medium;
 
 public class re_11 {
     public int maxArea(int[] height) {
-
+        return 0;
     }
 }

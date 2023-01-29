c596df5bffa5a3656a8d7166014148259ef0dff8
2giduck <cosmos7256@naver.com>
2023-01-29 22:31:44 +0900

    230129-2

---
+++ src/leetcode/medium/re2_238.java
@@ -0,0 +1,18 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class re2_238 {
+    public static void main(String[] args) {
+        int[] n1 = {1,2,3,4},
+        n2 = {-1, 1, 0, -1, 3};
+
+        System.err.println(Arrays.toString(productExceptSelf(n1)));
+        System.err.println(Arrays.toString(productExceptSelf(n2)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        return null;
+    }
+}
--- src/leetcode/medium/re_238.java
+++
@@ -1,7 +0,0 @@
-package leetcode.medium;
-
-public class re_238 {
-    public int[] productExceptSelf(int[] nums) {
-        return null;
-    }
-}

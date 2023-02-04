b305164deb682c208b95c2fa275fafb99ad7fcc7
2giduck <cosmos7256@naver.com>
2023-02-04 11:15:11 +0900

    230204-2

---
+++ src/leetcode/medium/re_146.java
@@ -0,0 +1,18 @@
+package leetcode.medium;
+
+public class re_146 {
+    class LRUCache {
+
+        public LRUCache(int capacity) {
+
+        }
+
+        public int get(int key) {
+            return 0;
+        }
+
+        public void put(int key, int value) {
+
+        }
+    }
+}

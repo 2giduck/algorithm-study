eacdf88f33117ac5b51875bdbcefe36f8e3ac7a5
2giduck <cosmos7256@naver.com>
2023-02-05 12:33:31 +0900

    230205-2

---
+++ src/leetcode/medium/_5.java
@@ -0,0 +1,13 @@
+package leetcode.medium;
+
+public class _5 {
+    public static void main(String[] args) {
+        String s1 = "babad", s2 = "cbbd";
+        System.err.println(longestPalindrome(s1));
+        System.err.println(longestPalindrome(s2));
+    }
+
+    public static String longestPalindrome(String s) {
+        return null;
+    }
+}

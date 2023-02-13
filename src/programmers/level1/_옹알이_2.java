cabf3b2c49bb4a3f1c48e0bb51e2d5b3f87089d1
2giduck <cosmos7256@naver.com>
2023-02-13 22:18:01 +0900

    230213-1

--- src/programmers/level1/_옹알이_2.java
+++ src/programmers/level1/_옹알이_2.java
@@ -1,10 +1,48 @@
 package programmers.level1;
 
-import java.util.*;
 
 public class _옹알이_2 {
-    public int solution(String[] babbling) {
+    public static void main(String[] args) {
+        String[] b1 = {"aya", "yee", "u", "maa"},
+                b2 = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        System.err.println(solution(b1));
+        System.err.println(solution(b2));
+    }
+
+    public static int solution(String[] babbling) {
         int answer = 0;
+
+        for(String b : babbling) {
+            if(getCount(b)) answer++;
+        }
+
         return answer;
+    }
+
+    public static boolean getCount(String babbling) {
+        String[] temp = {"aya", "ye", "woo", "ma"};
+
+        int prev = -1;
+        boolean check = false;
+        while(babbling.length() > 0) {
+            for (int i = 0; i < temp.length; i++) {
+                if(i == prev) continue;
+
+                if(babbling.startsWith(temp[i])) {
+                    prev = i;
+                    babbling = babbling.substring(temp[i].length());
+                    check = true;
+                    break;
+                }
+            }
+
+            if(check) {
+                check = false;
+            }else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

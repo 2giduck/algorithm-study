2600964ed960cd9c13c31c27a3c710e2f28c743a
2giduck <cosmos7256@naver.com>
2023-02-17 19:56:34 +0900

    230217-2

---
+++ src/leetcode/medium/re_17.java
@@ -0,0 +1,16 @@
+package leetcode.medium;
+
+import java.util.List;
+
+public class re_17 {
+    public static void main(String[] args) {
+        String d1 = "23", d2 = "", d3 = "2";
+        System.err.println(letterCombinations(d1));
+        System.err.println(letterCombinations(d2));
+        System.err.println(letterCombinations(d3));
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        return null;
+    }
+}

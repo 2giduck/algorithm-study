0ecb1512ea5ccac107cf86c06dc7f75f16a54947
2giduck <cosmos7256@naver.com>
2023-02-18 13:38:59 +0900

    230218-2

---
+++ src/leetcode/medium/re_139.java
@@ -0,0 +1,10 @@
+package leetcode.medium;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class re_139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return true;
+    }
+}

5024ee5990be597e39085b204053f8e47871a656
2giduck <cosmos7256@naver.com>
2023-02-19 17:36:17 +0900

    230219-1

---
+++ src/programmers/level1/re_햄버거_만들기.java
@@ -0,0 +1,14 @@
+package programmers.level1;
+
+public class re_햄버거_만들기 {
+    public static void main(String[] args) {
+        int[] i1 = {2, 1, 1, 2, 3, 1, 2, 3, 1},
+                i2 = {1, 3, 2, 1, 2, 1, 3, 1, 2};
+        System.err.println(solution(i1));
+        System.err.println(solution(i2));
+    }
+
+    public static int solution(int[] ingredient) {
+        return -1;
+    }
+}

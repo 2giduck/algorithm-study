6d3944675d424558923a38926b55230c6d69387a
2giduck <cosmos7256@naver.com>
2023-02-25 12:37:00 +0900

    230225-2

---
+++ src/baekjoon/silver/t3/re_1874.java
@@ -0,0 +1,14 @@
+package baekjoon.silver.t3;
+
+import java.util.*;
+
+public class re_1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = n-1; i >=0 ; i--) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}

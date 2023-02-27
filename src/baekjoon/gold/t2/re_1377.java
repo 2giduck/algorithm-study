fd54d2c63be10db4bcd59da714a634854d35abd5
2giduck <cosmos7256@naver.com>
2023-02-27 18:45:26 +0900

    230227-1

---
+++ src/baekjoon/gold/t2/re_1377.java
@@ -0,0 +1,14 @@
+package baekjoon.gold.t2;
+
+import java.util.Scanner;
+
+public class re_1377 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N+1];
+        for (int i = 1; i <= N ; i++) {
+            A[i] = sc.nextInt();
+        }
+    }
+}

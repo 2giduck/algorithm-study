ce89f9f98aef2f91baf6bb103d8e0eec04a8517a
2giduck <cosmos7256@naver.com>
2023-02-27 19:23:23 +0900

    230227-4

---
+++ src/baekjoon/silver/t5/re_11004.java
@@ -0,0 +1,16 @@
+package baekjoon.silver.t5;
+
+import java.util.Scanner;
+
+public class re_11004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    }
+}

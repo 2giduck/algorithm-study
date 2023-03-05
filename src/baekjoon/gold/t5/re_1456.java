eef9eaff65e1c3ff45800963c49642b6467af475
2giduck <41426853+2giduck@users.noreply.github.com>
2023-03-05 20:25:21 +0900

    230305-3

---
+++ src/baekjoon/gold/t5/re_1456.java
@@ -0,0 +1,15 @@
+package baekjoon.gold.t5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class re_1456 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+  }
+
+}

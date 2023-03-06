865c1fd01f2915735a3d407ddeea8aa88db785d9
2giduck <cosmos7256@naver.com>
2023-03-06 20:04:35 +0900

    230306-2

---
+++ src/baekjoon/gold/t1/re_1016.java
@@ -0,0 +1,13 @@
+package baekjoon.gold.t1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class re_1016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long min = sc.nextLong();
+        long max = sc.nextLong();
+    }
+}

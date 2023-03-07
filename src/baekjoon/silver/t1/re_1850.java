bf3f97a993b77c88cbcbdb9a496f214e74a7bdf6
2giduck <cosmos7256@naver.com>
2023-03-07 19:51:58 +0900

    230307-1

---
+++ src/baekjoon/silver/t1/re_1850.java
@@ -0,0 +1,11 @@
+package baekjoon.silver.t1;
+
+import java.util.Scanner;
+
+public class re_1850 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    }
+}

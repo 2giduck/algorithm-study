140e22204a365dd9b654bd1aa0c582993f8a8411
2giduck <41426853+2giduck@users.noreply.github.com>
2023-03-10 22:47:10 +0900

    230310-4

---
+++ src/baekjoon/gold/t4/re_1753.java
@@ -0,0 +1,16 @@
+package baekjoon.gold.t4;
+
+import java.util.Scanner;
+
+public class re_1753 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int V = sc.nextInt();
+    int E = sc.nextInt();
+    int K = sc.nextInt();
+
+
+
+  }
+}

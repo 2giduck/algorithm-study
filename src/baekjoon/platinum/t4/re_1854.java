75743fce817de7eb6d7128889c4a0785ece0958f
2giduck <41426853+2giduck@users.noreply.github.com>
2023-03-12 15:00:12 +0900

    230312-1

---
+++ src/baekjoon/platinum/t4/re_1854.java
@@ -0,0 +1,15 @@
+package baekjoon.platinum.t4;
+
+import java.util.Scanner;
+
+public class re_1854 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt(); // 도시 갯수
+    int m = sc.nextInt(); // 도로 수
+    int k = sc.nextInt();
+
+  }
+
+}

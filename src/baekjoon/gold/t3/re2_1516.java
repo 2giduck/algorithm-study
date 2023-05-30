d3cd2eaaf2942291aa0143f3dae832e139c7f8fb
2giduck <cosmos7256@naver.com>
2023-05-30 19:36:09 +0900

    230530

--- src/baekjoon/gold/t3/re2_1516.java
+++ src/baekjoon/gold/t3/re2_1516.java
@@ -1,14 +1,11 @@
 package baekjoon.gold.t3;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class re_1516 {
+public class re2_1516 {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
-
-    
   }
-
 }

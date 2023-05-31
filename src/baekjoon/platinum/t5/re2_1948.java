cf855a19ea03554c57c6bc24721a2aa10100a19e
2giduck <cosmos7256@naver.com>
2023-05-31 20:08:37 +0900

    230531

---
+++ src/baekjoon/platinum/t5/re2_1948.java
@@ -0,0 +1,13 @@
+package baekjoon.platinum.t5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class re2_1948 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+  }
+}
--- src/baekjoon/platinum/t5/re_1948.java
+++
@@ -1,9 +0,0 @@
-package baekjoon.platinum.t5;
-
-public class re_1948 {
-
-  public static void main(String[] args) {
-
-  }
-
-}

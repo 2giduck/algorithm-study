02d32adc4bbb2c33fac9296c04b4fccb867e2e0c
2giduck <cosmos7256@naver.com>
2023-07-06 22:24:16 +0900

    230706

--- src/baekjoon/silver/t3/_1463.java
+++ src/baekjoon/silver/t3/_1463.java
@@ -2,7 +2,7 @@ package baekjoon.silver.t3;
 
 import java.util.Scanner;
 
-public class re_1463 {
+public class _1463 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();

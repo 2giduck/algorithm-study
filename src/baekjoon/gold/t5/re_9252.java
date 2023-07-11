d25b3c8c3236b021d7fa5eb362e9d8d0d21e07f2
2giduck <cosmos7256@naver.com>
2023-07-11 22:21:33 +0900

    230711

---
+++ src/baekjoon/gold/t5/re_9252.java
@@ -0,0 +1,12 @@
+package baekjoon.gold.t5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class re_9252 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        var a = sc.next().toCharArray();
+        var b = sc.next().toCharArray();
+    }
+}

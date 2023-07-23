2725983d08d22f93da09a74edfb2ef85205f901d
2giduck <cosmos7256@naver.com>
2023-07-23 16:04:44 +0900

    230723

---
+++ src/baekjoon/bronze/t1/re_11050.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.t1;
+
+import java.util.Scanner;
+
+public class re_11050 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[][] d = new int[N+1][N+1];
+    }
+}

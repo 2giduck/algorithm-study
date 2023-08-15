463e548df684d73bfd9e90ef3e929b08f1d563f5
2giduck <cosmos7256@naver.com>
2023-08-15 22:33:09 +0900

    230815

---
+++ src/baekjoon/gold/t4/re2_17298.java
@@ -0,0 +1,20 @@
+package baekjoon.gold.t4;
+
+import java.util.Scanner;
+
+public class re2_17298 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+
+
+        System.out.println(sb);
+    }
+}
--- src/baekjoon/gold/t4/re_17298.java
+++
@@ -1,7 +0,0 @@
-package baekjoon.gold.t4;
-
-public class re_17298 {
-    public static void main(String[] args) {
-
-    }
-}

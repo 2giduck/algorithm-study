43aaf895b8dcd8df861a2ba2e5ce8e97c9ca5394
2giduck <cosmos7256@naver.com>
2023-08-17 22:33:13 +0900

    230817

--- src/baekjoon/silver/t4/_11399.java
+++ src/baekjoon/silver/t4/_11399.java
@@ -1,8 +1,9 @@
 package baekjoon.silver.t4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class re_11399 {
+public class _11399 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -10,5 +11,16 @@ public class re_11399 {
         for (int i = 0; i < N; i++) {
             p[i] = sc.nextInt();
         }
+
+        Arrays.sort(p);
+
+        int sum = 0;
+        int val = 0;
+        for(int i = 0; i<N; i++) {
+            sum += val + p[i];
+            val = val + p[i];
+        }// 1 2 3 3 4
+
+        System.out.println(sum);
     }
 }

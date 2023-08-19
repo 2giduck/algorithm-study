30cd5ef7ee9a8e934ed7b9262d4b4a44594aa245
2giduck <cosmos7256@naver.com>
2023-08-19 13:17:39 +0900

    230819

---
+++ src/programmers/level2/re_연속_부분_수열_합의_개수.java
@@ -0,0 +1,16 @@
+package programmers.level2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class re_연속_부분_수열_합의_개수 {
+    public static void main(String[] args) {
+        System.err.println(solution(new int[] {7, 9, 1, 1, 4}));
+    }
+
+    public static int solution(int[] elements) {
+        return 0;
+    }
+}

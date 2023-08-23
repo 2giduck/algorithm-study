542f5088d3d590c264ab463f52f2b5d991ceb61a
2giduck <cosmos7256@naver.com>
2023-08-23 21:44:42 +0900

    230823

---
+++ src/baekjoon/gold/t5/re_2023.java
@@ -0,0 +1,10 @@
+package baekjoon.gold.t5;
+
+import java.util.Scanner;
+
+public class re_2023 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}

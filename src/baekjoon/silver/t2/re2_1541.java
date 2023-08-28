0fefc6cbc1cb69f5bff3f7076821cfaaaee62ef9
2giduck <cosmos7256@naver.com>
2023-08-28 19:10:06 +0900

    230828

--- src/baekjoon/silver/t2/re2_1541.java
+++ src/baekjoon/silver/t2/re2_1541.java
@@ -1,11 +1,13 @@
 package baekjoon.silver.t2;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class re_1541 {
+public class re2_1541 {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
   }
-
 }

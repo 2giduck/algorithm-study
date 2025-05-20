de1b11eabc7f77c729e9785f45ccf7cc971a8a5f
2giduck <cosmos7256@naver.com>
2025-05-20 22:15:27 +0900

    250520-1

---
+++ src/baekjoon/silver/t5/re_11650.java
@@ -0,0 +1,14 @@
+package baekjoon.silver.t5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class re_11650 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+    }
+}

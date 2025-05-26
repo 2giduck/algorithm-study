ee262ab4462ac1c4fceed88cd3d7971e08254680
2giduck <cosmos7256@naver.com>
2025-05-26 20:32:12 +0900

    250526-2

---
+++ src/baekjoon/silver/t2/re2_6603.java
@@ -0,0 +1,12 @@
+package baekjoon.silver.t2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class re2_6603 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+}

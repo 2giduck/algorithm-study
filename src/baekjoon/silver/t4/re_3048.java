f1eb688bd3623990ca052c9b3d24a0aa952b2ff2
2giduck <41426853+2giduck@users.noreply.github.com>
2025-06-12 08:59:19 +0900

    Create re_3048.java

---
+++ src/baekjoon/silver/t4/re_3048.java
@@ -0,0 +1,19 @@
+package baekjoon.silver.t4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class re_3048 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int t = Integer.parseInt(br.readLine());
+    }
+}

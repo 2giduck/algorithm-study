0bf0b116fa0af6bb72f2237ae8ff71071055aac7
2giduck <41426853+2giduck@users.noreply.github.com>
2025-07-07 08:51:50 +0900

    Create re_18405.java

---
+++ src/baekjoon/gold/t5/re_18405.java
@@ -0,0 +1,21 @@
+package baekjoon.gold.t5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+public class re_18405 {
+
+    static ArrayList<Integer> list;
+    static int[][] arr;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+    }
+}

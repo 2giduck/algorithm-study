34e2e82cffb3ee514e87799f142cf014d16ba43e
2giduck <41426853+2giduck@users.noreply.github.com>
2025-07-15 08:52:21 +0900

    Create re_기둥과_보_설치.java

---
+++ src/programmers/level3/re_기둥과_보_설치.java
@@ -0,0 +1,13 @@
+package programmers.level3;
+
+public class re_기둥과_보_설치 {
+
+    public int[][] solution(int n, int[][] build_frame) {
+        // 0은 기둥, 1은 보
+        // 0은 삭제, 1은 설치
+
+
+        int[][] answer = {};
+        return answer;
+    }
+}

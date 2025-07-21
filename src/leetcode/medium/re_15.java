8bf7f40a1e5c1c1e891d6d18c17a9703d73f84c5
2giduck <41426853+2giduck@users.noreply.github.com>
2025-07-21 09:05:57 +0900

    Create re_15.java

---
+++ src/leetcode/medium/re_15.java
@@ -0,0 +1,14 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class re_15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> answer = new ArrayList<>();
+
+
+        return answer;
+    }
+}

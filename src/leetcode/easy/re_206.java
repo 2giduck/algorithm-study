479d1337b57bd7fe745133ef84c7f0d18b3f6c3a
2giduck <41426853+2giduck@users.noreply.github.com>
2025-07-31 08:58:24 +0900

    Create re_206.java

---
+++ src/leetcode/easy/re_206.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+public class re_206 {
+
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}

0c19799f5dd8f4f11172796128a431ecfacdb589
2giduck <41426853+2giduck@users.noreply.github.com>
2025-08-01 08:58:41 +0900

    Create re_33.java

---
+++ src/leetcode/medium/re_33.java
@@ -0,0 +1,13 @@
+package leetcode.medium;
+
+public class re_33 {
+
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        int answer = -1;
+        return answer;
+    }
+}

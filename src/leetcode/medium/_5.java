f9c121f16f048016e42470f34cb9f0340e18c9cb
2giduck <41426853+2giduck@users.noreply.github.com>
2025-08-05 08:55:04 +0900

    Update _5.java

--- src/leetcode/medium/_5.java
+++ src/leetcode/medium/_5.java
@@ -1,13 +1,39 @@
 package leetcode.medium;
 
 public class _5 {
-    public static void main(String[] args) {
-        String s1 = "babad", s2 = "cbbd";
-        System.err.println(longestPalindrome(s1));
-        System.err.println(longestPalindrome(s2));
-    }
+    public String longestPalindrome(String s) {
+        String max = String.valueOf(s.charAt(0));
 
-    public static String longestPalindrome(String s) {
-        return null;
+        for(int i = 0; i<s.length(); i++) {
+            for(int j = 0; j<s.length(); j++) {
+                int left = i - j;
+                int right = i + j;
+
+                if(left < 0 || right >= s.length()) break;
+                if(s.charAt(left) != s.charAt(right)) break;
+
+                if(max.length() < right - left + 1) {
+                    max = s.substring(left, right+1);
+                }
+            }
+        }
+
+        for(int i = 0; i<s.length() - 1; i++) {
+            if(s.charAt(i) != s.charAt(i+1)) continue;
+
+            for(int j = 0; j<s.length()/2; j++) {
+                int left = i - j;
+                int right = i+1 + j;
+
+                if(left < 0 || right >= s.length()) break;
+                if(s.charAt(left) != s.charAt(right)) break;
+
+                if(max.length() < right - left + 1) {
+                    max = s.substring(left, right+1);
+                }
+            }
+        }
+
+        return max;
     }
 }

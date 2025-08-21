60f0f8b9849e14d723bd41df1c28a2e1362c5727
2giduck <41426853+2giduck@users.noreply.github.com>
2025-08-21 09:23:25 +0900

    Create _374.java

---
+++ src/leetcode/easy/_374.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+public class _374 {
+
+    public int guessNumber(int n) {
+
+//        int left = 1;
+//        int right = n;
+//        int num = 1;
+//        while(left <= right) {
+//            num = left + (right - left) / 2;
+//            int temp = guess(num);
+//
+//            if(temp == 1) { // num < pick
+//                left = num + 1;
+//            }else if(temp == -1) {
+//                right = num - 1;
+//            }else {
+//                return num;
+//            }
+//        }
+
+        return -1;
+    }
+}

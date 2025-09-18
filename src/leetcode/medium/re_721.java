8873c25e45c636836b0303336280675923811d26
2giduck <41426853+2giduck@users.noreply.github.com>
2025-09-18 09:34:21 +0900

    Create re_721.java

---
+++ src/leetcode/medium/re_721.java
@@ -0,0 +1,11 @@
+package leetcode.medium;
+
+import java.util.List;
+
+public class re_721 {
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        return null;
+    }
+
+}

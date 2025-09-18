0c4fd89503b65b1b098f12e94ee4bc8c01853b34
2giduck <41426853+2giduck@users.noreply.github.com>
2025-09-19 08:36:52 +0900

    Create _198.java

---
+++ src/leetcode/medium/_198.java
@@ -0,0 +1,21 @@
+package leetcode.medium;
+
+public class _198 {
+
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        // dp[i] = i까지의 최대 훔친 돈
+        // dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i])
+
+        dp[0] = nums[0];
+        if(nums.length ==1) return dp[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i<nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+
+        }
+
+        return dp[nums.length-1];
+    }
+}

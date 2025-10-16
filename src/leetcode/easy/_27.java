ca3a4679d860d9582f9b54fa198d8e88f0a38ae6
2giduck <cosmos7256@naver.com>
2025-10-16 19:21:51 +0900

    251016

---
+++ src/leetcode/easy/_27.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+public class _27 {
+
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        return idx;
+    }
+}

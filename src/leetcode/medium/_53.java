d68079d4d53219719fbdbfd4e73c8f4ef04cfe7c
2giduck <cosmos7256@naver.com>
2026-03-04 21:32:06 +0900

    260304

--- src/leetcode/medium/_53.java
+++ src/leetcode/medium/_53.java
@@ -1,14 +1,14 @@
 package leetcode.medium;
 
 public class _53 {
-    public int maxSubArray(int[] nums) {
-        // dp[i] = Math.max(dp[i-1] + num[i], num[i]);
-        int size = nums.length;
-        int[] dp = new int[size];
 
+    public int maxSubArray(int[] nums) {
+        // dp[i] : i까지의 subarry중 합이 가장 큰 sum
+        // dp[i] = Max(dp[i-1] + nums[i], nums[i])
+        int[] dp = new int[nums.length];
         dp[0] = nums[0];
         int max = dp[0];
-        for(int i = 1; i < size; i++) {
+        for(int i = 1; i<nums.length; i++) {
             dp[i] = Math.max(dp[i-1] + nums[i] , nums[i]);
             max = Math.max(dp[i], max);
         }

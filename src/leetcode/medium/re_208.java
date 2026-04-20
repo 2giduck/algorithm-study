e5245b46ef718b818e4422e02e44d6f684176962
2giduck <cosmos7256@naver.com>
2026-04-20 19:22:44 +0900

    260420

--- src/leetcode/medium/re_208.java
+++ src/leetcode/medium/re_208.java
@@ -5,4 +5,24 @@ import java.util.Iterator;
 
 public class re_208 {
 
+    class Trie {
+
+        public Trie() {
+
+        }
+
+        public void insert(String word) {
+
+        }
+
+        public boolean search(String word) {
+            return true;
+        }
+
+        public boolean startsWith(String prefix) {
+            return true;
+        }
+    }
+
+
 }
